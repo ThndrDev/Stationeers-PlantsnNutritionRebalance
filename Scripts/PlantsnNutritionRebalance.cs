cfba235fde0aa3b1de1244879b2263906e63a9a7
ThndrDev <thunder.br@gmail.com>
2022-02-03 21:22:02 -0300

    Plants and Nutrition Rebalance

---
+++ Scripts/PlantsnNutritionRebalance.cs
@@ -0,0 +1,21 @@
+﻿using Stationeers.Addons;
+using Stationeers.Addons.API;
+using UnityEngine;
+using System;
+using System.IO;
+
+namespace PlantsnNutritionRebalance.Scripts
+{
+    public class PNRebalance : IPlugin
+    {
+        public void OnLoad()
+        {
+            Debug.Log("PlantsnNutritionRebalance: Mod is running!");
+        }
+        public void OnUnload()
+        {
+            Debug.Log("PlantsnNutritionRebalance: Bye!");
+
+        }
+    }
+}
